83c270bc5637a718bb48a5a633c1aefba4116761
im-sanny <raselparvezsanny@gmail.com>
2024-03-21 21:23:35 +0600

    setup for form master is done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SimpleForm from './Component/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+    <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,14 @@
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
